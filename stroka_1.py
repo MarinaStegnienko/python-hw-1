5e83a213145f5356445a60d2aa4fe25721538c7e
MarinaStegnienko <109163617+MarinaStegnienko@users.noreply.github.com>
2022-07-18 15:35:56 +0300

    Add files via upload

---
+++ stroka_1.py
@@ -0,0 +1,14 @@
+s=input("Введіть рядок: ")
+t=s[:5]
+print(t,'...')
+s1=s.upper()
+s2=s.lower()
+if s[0]=='L'or s[0]=='l':
+    print(s2)
+else:
+     if s[0]=='U' or s[0]=='u':
+        print(s1)
+        
+
+
+
